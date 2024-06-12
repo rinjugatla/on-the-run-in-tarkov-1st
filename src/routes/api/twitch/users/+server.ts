1e9a9f0b0528ee7b5f406ba0e912a07c28843119
rinjugatla <rin.jugatla@gmail.com>
2024-06-13 00:28:38 +0900

    デバッグログ出力を追加
    apiへのアクセスに失敗しているのでcloudflare上でのログの状態を確認

--- src/routes/api/twitch/users/+server.ts
+++ src/routes/api/twitch/users/+server.ts
@@ -3,14 +3,18 @@ import { json } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 
 export async function POST({request}): Promise<Response> {
+    console.log(request);
     const isSelfRequest = request.headers.get('sec-fetch-site') === 'same-origin';
     if (!isSelfRequest){ return json(new Response()); }
 
     const client = get(apiClient);
+    console.log(client);
 
     const { twitch_ids } = await request.json();
+    console.log(twitch_ids);
 
     const response = await client.users.getUsersByIds(twitch_ids);
+    console.log(response);
     const users =response.map(user => ({
         id: user.id,
         name: user.name,
@@ -22,5 +26,6 @@ export async function POST({request}): Promise<Response> {
         offlinePlaceholderUrl: user.offlinePlaceholderUrl,
         creationDate: user.creationDate
     }));
+    console.log(users);
     return json(users);
 }
